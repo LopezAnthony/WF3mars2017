10d33298ebd7299a422959ad125d587295ddbac9
Etudiant <Etudiant>
2017-03-28 11:17:40 +0200

    JS basics

---
+++ 28mars/exo3/js/script.js
@@ -0,0 +1,11 @@
+//  Créer une variable de type STRING
+var myString = 'Je suis une string';
+console.log(myString);
+
+//  Créer une variable de type NUMBER (INTEGER)
+var myNumber = 26;
+console.log(myNumber);
+
+//  Créer une variable de type BOOLEAN : true/false
+var myBoolean = true;
+console.log(myBoolean);
---
+++ 28mars/exo5/js/script.js
@@ -0,0 +1,20 @@
+//  Créer une variable de type NUMBER avec la valeur 10
+
+// Opérateur d'affectation 
+var myNumber = 10;
+console.log(myNumber);
+
+//  Opérateur d'incrémentation : ++
+myNumber++;
+console.log(myNumber);
+
+//  Opérateur de décrémentation : --
+myNumber--;
+console.log(myNumber);
+
+//  Opérateur d'addition/concaténation : +
+var myAdd = 7 + 5;
+console.log(myAdd);
+
+//  Opérateur de soustraction : -
+var mySous
