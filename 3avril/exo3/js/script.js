ec6c577bcf3ae1280e13b40bfdeb4cc1b9069fad
Etudiant <Etudiant>
2017-04-03 16:56:15 +0200

    formulaire complexe

--- 3avril/exo3/js/script.js
+++ 3avril/exo3/js/script.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){    
 
+    // Créer un tableau pour enregistrer les avatars
+    var myTown = [];
+
 
     // Vérifier le genre de l'avatar
         var avatarWoman = $('#avatarWoman');
@@ -15,6 +18,12 @@ $(document).ready(function(){
 
                 // Modifier la valeur de avatarGender
                 avatarGender = avatarWoman.val();
+
+                // Vider le message d'erreur
+                $('.labelGender b').text('');
+
+                // Modifier l'attribut src de #avatarBody
+                $('#avatarBody').attr('src', 'img/' + avatarGender + '.png');
             });
 
         // => avatarMan capter le click
@@ -26,7 +35,30 @@ $(document).ready(function(){
                 // Modifier la valeur de avatarGender
                 avatarGender = avatarMan.val();
 
+                // Vider le message d'erreur
+                $('.labelGender b').text('');
+
+                // Modifier l'attribut src de #avatarBody
+                 $('#avatarBody').attr('src', 'img/' + avatarGender + '.png');
+
             });
+
+        // Capter l'événement change() sur les selects
+        $('select').change(function(){
+            
+            // Condition if pour modifier la valeur src de avatarTop ou avatarBottom
+            if ($(this).attr('id') == 'avatarStyleTop') {
+                
+                // Modifier la valeur de l'attribut src de #avatarTop
+                $('#avatarTop').attr('src', 'img/top/' + $(this).val()+ '.png');
+            } else {
+                
+                // Modifier la valeur de l'attribut src de #avatarTop
+                $('#avatarBottom').attr('src', 'img/bottom/' + $(this).val()+ '.png');
+            };
+
+        });
+        
 
             
 
@@ -50,49 +82,54 @@ $(document).ready(function(){
 
         // => avatarName Minimum 5 caractères
         if (avatarName.length < 5) {
-            console.log('Min. 5 caractères');
-        } else {
-            console.log('avatarName OK');
 
+            // Ajouter un message d'erreur dans le label du dessus
+            $('[for="avatarName"] b').text('Min. 5 caractères')
+        } else {
+            
             // Incrémenter la variable formScore
             formScore++
         };
 
         //  => avatarAge entre 1 et 100
         if ( avatarAge == 0 || avatarAge > 100 || avatarAge.length == 0 ) {
-            console.log('fail')
-        } else {
-            console.log('avatarAge OK');
 
+            // Ajouter un message d'erreur dans le label du dessus
+            $('[for="avatarAge"] b').text('Entre 1 et 100')
+        } else {
+           
             // Incrémenter la variable formScore
             formScore++;
         };
 
         //  => avatarStyleTop obligatoire
         if(avatarStyleTop == 'null'){
-            console.log('Vous de vez choisir un avatarStyleTop')
+            // Ajouter un message d'erreur dans le label du dessus
+            $('[for="avatarStyleTop"] b').text('Sélectionner un haut')
         }else{
-            console.log('avatarStyleTop OK');
-
+            
             // Incrémenter la variable formScore
             formScore++;
+
+
+
         };
 
         //  => avatarStyleBottom obligatoire
         if(avatarStyleBottom == 'null'){
-            console.log('Vous de vez choisir un avatarStyleBottom')
+            // Ajouter un message d'erreur dans le label du dessus
+            $('[for="avatarStyleBottom"] b').text('Sélectionner un bas')
         }else{
-            console.log('avatarStyleBottom OK');
-
+           
             // Incrémenter la variable formScore
             formScore++;
         };
 
         // => avatarGender vérifier la valeur
         if( avatarGender == undefined ){
-            console.log('Vous devez choisir un genre')
+            // Ajouter un message d'erreur dans le label du dessus
+            $('.labelGender b').text('Sélectionner un genre')
         }else{
-            console.log('avatarGender OK');
 
             // Incrémenter la variable formScore
             formScore++;
@@ -100,13 +137,89 @@ $(document).ready(function(){
 
         // => Vérifier la valeur de formScore
         if ( formScore == 5) {
-            console.log('formScore OK');
+
+            //  => Envoyer les données du formulaire et attendre la réponse du server en Ajax
+            // => Le server répond true
+
+                // Ajouter une ligne dans le tableau
+                $('table').append('' +
+                    '<tr>'+
+                        '<td><b>'+ avatarName + '</b></td>'+
+                        '<td>'+avatarAge+'</td>'+
+                        '<td>'+avatarGender+'</td>'+
+                        '<td>'+ avatarStyleTop + ', '+ avatarStyleBottom +'</td>'  +                                                          
+                    '</tr>'
+                );
+
+                // Ajouter l'avatar dans le tableau JS
+                myTown.push({
+                    name: avatarName,
+                    age: parseInt(avatarAge),
+                    gender: avatarGender,
+                    top: avatarStyleTop,
+                    bottom: avatarStyleBottom
+                });
+
+                // Vider les champs du formulaire
+                $('form')[0].reset();
+
+                // Revenir aux valeurs 'null' pour l'avatar
+                $('#avatarBody').attr('src', 'img/null.png');
+                $('#avatarTop').attr('src', 'img/top/null.png');
+                $('#avatarBottom').attr('src', 'img/bottom/null.png');
+
+                // Afficher les données du tableau JS dans la console
+                console.log( myTown.length );
+
+                // Afficher la taille totale de la ville dans #totalSims
+                $('#totalSims').text(myTown.length);
+
+                // Définir les variables glabales pour les statistiques
+                var totalGirls = 0;
+                var totalBoys = 0;
+                var totalAge = 0;
+
+                // Faire une boucle sur myTown pour connaître les statistiques
+                for (var i = 0; i < myTown.length; i++) {
+
+                    console.log( myTown[i].gender);
+
+                    // Condition pour le gender
+                    if (myTown[i].gender == 'girl') {
+                        totalGirls++
+                    } else{
+                        totalBoys++
+                    }
+
+                    // Additionner les âges
+                    totalAge += myTown[i].age 
+
+                };
+
+                // Afficher dans le tableau HTML le nombre de girls et le nombre de totalBoys
+                $('#simsWoman').html(totalGirls + '<b>/' + myTown.length +'</b>');
+                $('#simsMan').html(totalBoys + '<b>/' + myTown.length +'</b>');
+
+                // Afficher l'âge total dans la console
+                var ageAmount = Math.round(totalAge/myTown.length);
+                $('#simsAgeAmount').html(ageAmount +  ' <b>ans</b>');
+                
+                
+                
         }else{
             console.log('formScore not OK!');
         };
 
 
     }); // Fin du form submit  
+
+    // Supprimer les messages d'erreur au focus
+    $('input, select').focus(function(){
+        // Sélectionner la balise précédente de input
+        $(this).prev().children('b').text('');
+
+
+    });
 
 
 }); //Fin chargement du DOM
