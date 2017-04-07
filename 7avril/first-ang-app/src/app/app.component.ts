5e2cede7ec2d1b49621b3560b31aa91de1b6875b
Etudiant <Etudiant>
2017-04-07 16:08:26 +0200

    intro angular2

--- 7avril/first-ang-app/src/app/app.component.ts
+++ 7avril/first-ang-app/src/app/app.component.ts
@@ -1,5 +1,8 @@
 // Importer la classe Component
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+// Importer la class router
+import { Router } from '@angular/router';
 
 // Définir le décorateur @Component({...})
 @Component({
@@ -9,16 +12,52 @@ import { Component } from '@angular/core';
 })
 
 // Export la class du composant
-export class AppComponent {
-  title = 'Brand New App';
-  collection =['Pierre', 'Paul', 'Jacques', 'Anthony']
-  error = 'pasCool'
-  openAlert(){
-    alert('Salut')
-  }
-  sayHelloToUser(user){
-    console.log('Hello ' + user)
+export class AppComponent implements OnInit{
+
+  // Initier le router dans le constructeur du composant
+  constructor(
+    private router : Router
+  ){}
+
+  burgerIsOpen = false;
+
+  // Créer une fonction à appeler au click sur .fa-bars
+  openBurger(){
+
+    if(this.burgerIsOpen == false){
+
+    // Changer la valeur de burgerIsOpen
+    this.burgerIsOpen = true;
+
+    }else{
+
+      // Changer la valeur de burgerIsOpen
+    this.burgerIsOpen = false;
+
+    }
+
+
+  };
+
+  // Créer une fonction pour fermer le burger menu
+  closeBurger(link){
+
+    // Fermer le burger menu
+    this.burgerIsOpen = false;
+
+    // Naviguer vers la bonne vue
+    this.router.navigate([link]);
+
   }
 
-  awesome = "Paulo"
+  // Attendre le chargement du composant
+  ngOnInit(){
+
+    // Créer une variable pour séléctionner le loader en JavaScript
+    let loader = document.getElementById('appLoader');
+
+    // Ajouter la class closedLoader à la balise
+    loader.classList.add('closedLoader');
+  }
+
 }
--- 7avril/first-ang-app/src/app/app.module.ts
+++ 7avril/first-ang-app/src/app/app.module.ts
@@ -3,16 +3,28 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+// Importer la class du module Routing
+import { Router } from "./app.routing";
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    PortfolioComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    Router
   ],
   providers: [],
   bootstrap: [AppComponent]
