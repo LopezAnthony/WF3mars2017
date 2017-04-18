e4376665a75677e4f48bc7005dc47cb6f8221195
Etudiant <Etudiant>
2017-04-18 17:20:25 +0200

    SQL course

--- SQL/06transactions/transactions.sql
+++ SQL/06transactions/transactions.sql
@@ -16,3 +16,20 @@
         COMMIT; --Valide l'ensemble de la transaction
 
         -- Si on ne fait ni ROLLBACK ni COMMIT avant la déconnexion au SGBD, c'est un ROLLBACK qui s'effctue par défaut.
+
+    --Transaction avancée:
+        START TRANSACTION;
+            SAVEPOINT point1; --point de sauvegarde appelé point1
+            UPDATE employes SET prenom = 'julien A' WHERE id_employes = 699;
+            SAVEPOINT point2; --point de sauvegarde appelé point2
+            UPDATE employes SET prenom = 'julien B' WHERE id_employes = 699;
+
+        ROLLBACK TO point2; --pour annuler une partie de la transaction : retour au point 2 => on garde 'julien A' en base.
+
+            --ou bien:
+
+        ROLLBACK; --pour annuler toute la transaction => on garde 'Julien' en base
+
+            --ou bien:
+
+        COMMIT; --pour valider les opérations de la transaction
---
+++ SQL/07variables/variables.sql
@@ -0,0 +1,16 @@
+--***************************************
+-- Les VARIABLES en SQL
+--***************************************
+
+    --Une variable est un espace mémoire nommé qui permet de conserver une valeur.
+
+        --Permet d'observer les variables système:
+        SHOW VARIABLES;
+
+        SELECT @@version; --on met deux @ pour accèder à une variable système
+
+    --Déterminer nos propres variables :
+        SET @ecole = 'WF3'; --déclare une variable ecoler et lui affecte la valeur 'WF3'.
+        SELECT @ecole; --on peut accéder au contenu de cette variable par son nom.
+
+        
